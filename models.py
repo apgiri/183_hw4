2d832cbd16a754e7d110e35dbd528f8c110a06b3
apgiri <apgiri@ucsc.edu>
2021-04-24 23:43:48 -0700

    added a new table that references contact

--- models.py
+++ models.py
@@ -19,5 +19,20 @@ def get_time():
 # db.define_table('thing', Field('name'))
 #
 ## always commit your models to avoid problems later
+db.define_table(
+    'contact',
+    Field('first_name'),
+    Field('last_name'),
+    Field('user_email', default=get_user_email),
+)
+db.contact.id.readable = db.contact.id.writable = False
+db.contact.user_email.readable = db.contact.user_email.writable = False
+
+db.define_table(
+    'phone',
+    Field('contact_id', 'reference contact'),
+    Field('number'),
+    Field('phone_type')
+)
 
 db.commit()
